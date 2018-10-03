3d979ee652a85ee74f35a5c08d1f591ba93b8e3f
Orne <Orne@EXO>
2018-10-03 18:24:16 -0300

    Se carga el archivo calculadora
    realiza operaciones basicas

---
+++ taismaven/src/main/java/com/orne/taismaven/Calculadora.java
@@ -0,0 +1,21 @@
+package com.orne.taismaven;
+
+public class Calculadora {
+	
+	public int suma (int a, int b) {
+		return a+b;
+		}
+	
+	public int resta (int a, int b) {
+		return b-a;
+	}
+	
+	public int producto (int a, int b) {
+		return a*b;
+	}
+	
+	public double divide (double a, double b) {
+		return a/b;
+	}
+
+}
